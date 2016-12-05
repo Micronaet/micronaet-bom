2743ae5148388f79ea1016ca19bc693b3065df99
nicothebrush <nicola.riolini@gmail.com>
2016-12-05 12:22:07 +0100

    add script for export inventory not in list

--- temp_script/__init__.py
+++ temp_script/__init__.py
@@ -18,6 +18,6 @@
 #
 ###############################################################################
 from . import script
-
+from . import inventory
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ temp_script/inventory.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Copyright (C) 2001-2014 Micronaet SRL (<http://www.micronaet.it>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+import os
+import sys
+import logging
+import openerp
+import openerp.netsvc as netsvc
+import openerp.addons.decimal_precision as dp
+from openerp.osv import fields, osv, expression, orm
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+from openerp import SUPERUSER_ID, api
+from openerp import tools
+from openerp.tools.translate import _
+from openerp.tools.float_utils import float_round as round
+from openerp.tools import (DEFAULT_SERVER_DATE_FORMAT, 
+    DEFAULT_SERVER_DATETIME_FORMAT, 
+    DATETIME_FORMATS_MAP, 
+    float_compare)
+
+from inventory_present import product_present
+
+_logger = logging.getLogger(__name__)
+
+class ResCompany(orm.Model):
+    """ Model name: ResCompany
+    """
+    _inherit = 'res.company'
+
+    # Procedure:    
+    def inventory_to_reset(self, cr, uid, ids, context=None):
+        ''' Check error during operation on bom
+        '''
+        out_file = '/home/administrator/photo/log/product_to_reset'
+        out_file_jump = '/home/administrator/photo/log/product_to_reset_jumped'
+        
+        out_f = open(out_file, 'w')
+        out_jump = open(out_file_jump, 'w')
+        
+        # Pool used:
+        product_pool = self.pool.get('product.product')
+
+        # Check HW bon link:
+        product_ids = product_pool.search(cr, uid, [
+            ('default_code', 'not in', product_present),                     
+            ], context=context)
+
+        for product in product_pool.browse(
+                cr, uid, product_ids, context=context):                
+            if not product.default_code or product.default_code[:1] not in \
+                    '1234567890G':                    
+                out_jump.write('%s|0\n' % product.default_code)
+                continue     
+                    
+            out_f.write('%s|0\n' % product.default_code)
+        out_f.close()
+        out_jump.close()        

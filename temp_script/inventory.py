b1643c2c072f136d900a97eb11ef442e473d1df1
nicothebrush <nicola.riolini@gmail.com>
2017-01-23 16:01:03 +0100

    export all

--- temp_script/inventory.py
+++ temp_script/inventory.py
@@ -52,17 +52,15 @@ class ResCompany(orm.Model):
         _logger.warning('Export lavoration problem: %s' % out_file)
         
         pick_pool = self.pool.get('stock.picking')
-        pick_ids = pick_pool.search(cr, uid, [
-            ('create_date', '>=', '2017-01-01 00:00:00'),
-            ('date', '<', '2017-01-01'),
-            ], context=context)
+        pick_ids = pick_pool.search(cr, uid, [], context=context)
             
         for pick in pick_pool.browse(cr, uid, pick_ids, context=context):
             for line in pick.sl_quants_ids:
-                out_f.write('%s|%s|%s|%s|%s\n' % (
+                out_f.write('%s|%s|%s|%s|%s|%s\n' % (
                     # Header:
                     pick.name,
                     pick.create_date,
+                    pick.write_date,
                     pick.date,
                     # Line:
                     line.product_id.default_code,

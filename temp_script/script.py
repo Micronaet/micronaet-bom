2fef2105c7b51b5c9c95654b78a8168855c29697
nicothebrush <nicola.riolini@gmail.com>
2017-03-30 11:19:19 +0200

    fast git

--- temp_script/script.py
+++ temp_script/script.py
@@ -83,13 +83,17 @@ class ResCompany(orm.Model):
             except:
                 import pdb; pdb.set_trace()    
                 continue
-            p_ids = product_pool.search(cr, uid, [
-                ('default_code', '=', default_code),
-                ], context=context)
-            if not p_ids:
-                _logger.error('Not found %s' % default_code)
+            try:
+                p_ids = product_pool.search(cr, uid, [
+                    ('default_code', '=', default_code),
+                    ], context=context)
+                if not p_ids:
+                    _logger.error('Not found %s' % default_code)
+                    continue
+                product_ids.append(p_ids[0])
+            except:
+                import pdb; pdb.set_trace()    
                 continue
-            product_ids.append(p_ids[0])
         
         
         # Create record if not present in product

24cd933bca92d14d0af382948c6d84dc118e95f3
nicothebrush <nicola.riolini@gmail.com>
2017-09-12 10:30:54 +0200

    fast git

--- mrp_direct_line/apache/www/linea.php
+++ mrp_direct_line/apache/www/linea.php
@@ -1,7 +1,8 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <?php
-    include('xmlrpc/xmlrpc.inc');
     include('config.php');
+    die($server_url);
+    include('xmlrpc/xmlrpc.inc');
 
     // Show error:
     ini_set('display_errors', 'On');

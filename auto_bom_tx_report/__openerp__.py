21059cf8e7f6890ce639983badfbb255d9fe48f9
nicothebrush <nicola.riolini@gmail.com>
2017-09-25 09:14:28 +0200

    add dep for create description

--- auto_bom_tx_report/__openerp__.py
+++ auto_bom_tx_report/__openerp__.py
@@ -33,6 +33,7 @@
         'purchase', # For create operation from XLSX
         'order_bom_explode_report', # report and wizard
         'textilene_status', # only for group management TODO extract group!
+        'purchase_line_description',
         ],
     'init_xml': [],
     'demo': [],

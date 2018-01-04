f30a554d3dff8c1ed141dae747210f60648d871b
nicothebrush <nicola.riolini@gmail.com>
2018-01-04 11:16:50 +0100

    correct net HW using MRP data

--- order_bom_explode_report/report/explode_parser.py
+++ order_bom_explode_report/report/explode_parser.py
@@ -167,7 +167,7 @@ class MrpProduction(orm.Model):
             if product.default_code not in hw_fabric:
                 # Create empty record with fixed data:
                 hw_fabric[product.default_code] = [
-                    product.mx_net_qty, # 0. Stock HW XXX use mx_net_mrp_qty?
+                    product.mx_net_mrp_qty, # 0. Stock (Before: mx_net_qty)
                     0.0, # 1. OC remain HW
                     0.0, # 2. Stock Component (mt  of fabric)
                     #mt, # 3. Mt. from BOM

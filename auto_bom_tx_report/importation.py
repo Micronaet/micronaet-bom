0caf94c89935170c0c28dd8760da77373ea7d41d
nicothebrush <nicola.riolini@gmail.com>
2020-05-22 17:03:15 +0200

    pep8 and start import procedure

--- auto_bom_tx_report/importation.py
+++ auto_bom_tx_report/importation.py
@@ -32,17 +32,18 @@ from openerp import SUPERUSER_ID, api
 from openerp import tools
 from openerp.tools.translate import _
 from openerp.tools.float_utils import float_round as round
-from openerp.tools import (DEFAULT_SERVER_DATE_FORMAT, 
-    DEFAULT_SERVER_DATETIME_FORMAT, 
-    DATETIME_FORMATS_MAP, 
+from openerp.tools import (DEFAULT_SERVER_DATE_FORMAT,
+    DEFAULT_SERVER_DATETIME_FORMAT,
+    DATETIME_FORMATS_MAP,
     float_compare)
 
 
 _logger = logging.getLogger(__name__)
 
+
 class PurchaseOrderXLSX(orm.Model):
     """ Model name: PurchaseOrderXLSX
-    """    
+    """
     _name = 'purchase.order.xlsx'
     _description = 'Imported order'
     _rec_name = 'name'
@@ -52,20 +53,20 @@ class PurchaseOrderXLSX(orm.Model):
     # Wizard button event:
     # --------------------
     def extract_line_in_tree(self, cr, uid, ids, context=None):
-        ''' Extract element in list
-        '''
+        """ Extract element in list
+        """
         model_pool = self.pool.get('ir.model.data')
         view_id = model_pool.get_object_reference(
             cr, uid, 'auto_bom_tx_report', 'view_order_line_tree')[1]
         current_proxy = self.browse(cr, uid, ids, context=context)[0]
         line_ids = [item.id for item in current_proxy.line_ids]
-        
+
         return {
             'type': 'ir.actions.act_window',
             'name': _('Righe'),
             'view_type': 'form',
             'view_mode': 'tree',
-            #'res_id': 1,
+            # 'res_id': 1,
             'res_model': 'purchase.order.xlsx.line',
             'view_id': view_id,
             'views': [(view_id, 'tree')],
@@ -74,26 +75,26 @@ class PurchaseOrderXLSX(orm.Model):
             'target': 'current', # 'new'
             'nodestroy': False,
             }
-            
+
     def action_create_order(self, cr, uid, ids, context=None):
-        ''' Create purchase order:
-        '''
-        
+        """ Create purchase order:
+        """
+
         # Pool used:
         purchase_pool = self.pool.get('purchase.order')
         pol_pool = self.pool.get('purchase.order.line')
         location_pool = self.pool.get('stock.location')
-        
+
         today = datetime.now().strftime(DEFAULT_SERVER_DATETIME_FORMAT)
-        purchase_db = {} # list of purchase order per partner
+        purchase_db = {}  # list of purchase order per partner
         for line in self.browse(cr, uid, ids, context=context)[0].line_ids:
             partner = line.partner_id
             if not partner.id:
                 raise osv.except_osv(
-                    _('No partner'), 
+                    _('No partner'),
                     _('Set all partner for create invoice!'),
                     )
-                
+
             # -----------------------------------------------------------------
             # Header:
             # -----------------------------------------------------------------
@@ -103,21 +104,22 @@ class PurchaseOrderXLSX(orm.Model):
                     ('name', '=', 'Stock'),
                     ], context=context)
 
-                res = purchase_pool.onchange_partner_id(cr, uid, False, 
+                res = purchase_pool.onchange_partner_id(
+                    cr, uid, False,
                     partner.id, context=context)
                 data = res.get('value', {})
-                    
+
                 # Generate header:
                 data.update({
                     'partner_id': partner.id,
                     'date_order': today,
                     'location_id': location_ids[0],
                     })
-                purchase_id = purchase_pool.create(cr, uid, data, 
-                    context=context)
+                purchase_id = purchase_pool.create(
+                    cr, uid, data, context=context)
                 purchase_db[partner.id] = purchase_pool.browse(
                     cr, uid, purchase_id, context=context)
-                    
+
             # -----------------------------------------------------------------
             # Details:
             # -----------------------------------------------------------------
@@ -135,39 +137,42 @@ class PurchaseOrderXLSX(orm.Model):
             date_planned = line.deadline
             qty = line.quantity
             uom_id = line.product_id.uom_id.id
-            
-            res = pol_pool.onchange_product_id(cr, uid, False, 
+
+            res = pol_pool.onchange_product_id(
+                cr, uid, False,
                 pricelist_id, product_id, 0, False, partner_id, date_order,
-                fiscal_position, date_planned, False, False, 'draft', 
+                fiscal_position, date_planned, False, False, 'draft',
                 context=context)
             data_line.update(res.get('value', {}))
-                        
+
             # onchange product_qty:
-            res = pol_pool.onchange_product_id(cr, uid, False,
-                pricelist_id, product_id, qty, uom_id, partner_id, date_order, 
-                fiscal_position, date_planned, name, False, 'draft', 
+            res = pol_pool.onchange_product_id(
+                cr, uid, False,
+                pricelist_id, product_id, qty, uom_id, partner_id, date_order,
+                fiscal_position, date_planned, name, False, 'draft',
                 context=context)
             data_line.update(res.get('value', {}))
-            
+
             # onchange uom:
-            res = pol_pool.onchange_product_uom(cr, uid, False,
+            res = pol_pool.onchange_product_uom(
+                cr, uid, False,
                 pricelist_id, product_id, qty, uom_id, partner_id, date_order,
-                fiscal_position, date_planned, name, False, 'draft', 
-                context=context) 
+                fiscal_position, date_planned, name, False, 'draft',
+                context=context)
             data_line.update(res.get('value', {}))
 
             if 'taxes_id' in data_line:
                 taxes_id = [(6, 0, data_line['taxes_id'])]
-                
+
             data_line.update({
                 'product_id': product_id,
                 'order_id': purchase_id,
                 'taxes_id': taxes_id,
                 # TODO link link with file?
                 })
-            
+
             pol_pool.create(cr, uid, data_line, context=context)
-                    
+
         # Link purchase order to import record
         purchase_ids = [po.id for po in purchase_db.values()]
         purchase_pool.write(cr, uid, purchase_ids, {
@@ -176,48 +181,48 @@ class PurchaseOrderXLSX(orm.Model):
         return True
 
     def action_import_leadtime_lot(self, cr, uid, ids, context=None):
-        ''' Event for button done force update lead lot
-        '''
-        if context is None: 
+        """ Event for button done force update lead lot
+        """
+        if context is None:
             context = {}
-        
+
         context['update_lead_lot'] = True
         context['update_inventory_category'] = True
         context['obsolete'] = True
-        
+
         return self.action_import_order(cr, uid, ids, context=context)
 
     def action_import_order(self, cr, uid, ids, context=None):
-        ''' Event for button done
-        '''
-        if context is None: 
-            context = {}        
-        
+        """ Event for button done
+        """
+        if context is None:
+            context = {}
+
         update_lead_lot = context.get('update_lead_lot', False)
         update_inventory_category = context.get(
             'update_inventory_category', False)
         update_obsolete = context.get('obsolete', False)
-        
+
         # Pool used:
         product_pool = self.pool.get('product.product')
-        line_pool = self.pool.get('purchase.order.xlsx.line') 
+        line_pool = self.pool.get('purchase.order.xlsx.line')
         inventory_pool = self.pool.get('product.product.inventory.category')
-        
+
         inventory_db = {}
         if update_inventory_category:
             inventory_ids = inventory_pool.search(cr, uid, [], context=context)
-            for item in inventory_pool.browse(cr, uid, inventory_ids, 
+            for item in inventory_pool.browse(cr, uid, inventory_ids,
                     context=context):
                 inventory_db[item.name] = item.id
-            
+
         current_proxy = self.browse(cr, uid, ids, context=context)[0]
-        
+
         # ---------------------------------------------------------------------
         # Save file passed:
         # ---------------------------------------------------------------------
         if not current_proxy.file:
             raise osv.except_osv(
-                _('No file:'), 
+                _('No file:'),
                 _('Please pass a XLSX file for import order'),
                 )
         b64_file = base64.decodestring(current_proxy.file)
@@ -226,19 +231,19 @@ class PurchaseOrderXLSX(orm.Model):
         f = open(filename, 'wb')
         f.write(b64_file)
         f.close()
-        
+
         xslx_id = current_proxy.id
         month_db = {
             # Previous year:
             2: '09', 3: '10', 4: '11', 5: '12',
-            
+
             # Next year:
-            6: '01', 7: '02', 8: '03', 9: '04', 10: '05', 11: '06', 
+            6: '01', 7: '02', 8: '03', 9: '04', 10: '05', 11: '06',
             12: '07', 13: '08',
             }
-            
-        # Parameters:    
-        row_start = 0            
+
+        # Parameters:
+        row_start = 0
         month_current = datetime.now().month
         year_current =  datetime.now().year
         if month_current in [9, 10, 11, 12]:
@@ -247,7 +252,7 @@ class PurchaseOrderXLSX(orm.Model):
         else: # 1 > 8
             year_a = year_current - 1
             year_b = year_current
-            
+
         # ---------------------------------------------------------------------
         # Load force name (for web publish)
         # ---------------------------------------------------------------------
@@ -255,35 +260,35 @@ class PurchaseOrderXLSX(orm.Model):
             WB = xlrd.open_workbook(filename)
         except:
             raise osv.except_osv(
-                _('Error XLSX'), 
+                _('Error XLSX'),
                 _('Cannot read XLS file: %s' % filename),
                 )
-        
-        # ---------------------------------------------------------------------        
+
+        # ---------------------------------------------------------------------
         # Loop on all pages:
-        # ---------------------------------------------------------------------        
+        # ---------------------------------------------------------------------
         today = now[:10]
         for ws_name in WB.sheet_names():
             if ws_name == 'Non usati':
                 _logger.warning('Jump page: %s' % ws_name)
                 continue
-                
-            WS = WB.sheet_by_name(ws_name)   
+
+            WS = WB.sheet_by_name(ws_name)
             _logger.warning('Read page: %s' % ws_name)
 
             pos = -1
             error = ''
             for row in range(row_start, WS.nrows):
                 pos += 1
-                if pos == 1: 
+                if pos == 1:
                     # ---------------------------------------------------------
                     # Read product code:
                     # ---------------------------------------------------------
                     default_code = WS.cell(row, 0).value
                     _logger.info('Find material: %s' % default_code)
 
-                    product_id = False           
-                      
+                    product_id = False
+
                     # Manage code error:
                     if not default_code:
                         _logger.error('No material code')
@@ -294,30 +299,30 @@ class PurchaseOrderXLSX(orm.Model):
                     product_ids = product_pool.search(cr, uid, [
                         ('default_code', '=', default_code)
                         ], context=context)
-                        
+
                     # Manage product error:
-                    if not product_ids: 
+                    if not product_ids:
                         _logger.error(
                             'No product with code: %s' % default_code)
                         # FATAL ERROR (maybe file not in correct format, raise:
                         raise osv.except_osv(
-                            _('Errore controllare anche il formato del file'), 
+                            _('Errore controllare anche il formato del file'),
                             _('Non trovato il codice prodotto: %s' % (
                                 default_code)),
                             )
                         error += 'Riga: %s > No prodotto: %s\n' % (
                             row, default_code)
                         continue
-                        
+
                     # TODO manage warning more than one product
                     elif len(product_ids) > 1:
                         _logger.error('More material code: %s' % default_code)
                         error += 'Riga: %s > Codice doppio: %s\n' % (
                             row, default_code)
                         pass # TODO multi code
-                    
-                    product_id = product_ids[0]    
-                    product_proxy = product_pool.browse(cr, uid, product_id, 
+
+                    product_id = product_ids[0]
+                    product_proxy = product_pool.browse(cr, uid, product_id,
                         context=context)
 
                     # ---------------------------------------------------------
@@ -326,20 +331,20 @@ class PurchaseOrderXLSX(orm.Model):
                     # 1. Procurements:
                     if product_proxy.seller_ids:
                         partner_id = product_proxy.seller_ids[0].name.id
-                    
+
                     # 2. First supplier
                     elif product_proxy.first_supplier_id:
                         partner_id = product_proxy.first_supplier_id.id
-                    else:   
-                        partner_id = False                    
-                        
-                elif pos == 6: # order qty                
+                    else:
+                        partner_id = False
+
+                elif pos == 6: # order qty
                     # ---------------------------------------------------------
                     # Read quantity and get deadline:
                     # ---------------------------------------------------------
                     for col in range(2, 14):
                         quantity = WS.cell(row, col).value
-                        
+
                         try:
                             # TODO replace . on , ?
                             quantity = float(quantity)
@@ -347,10 +352,10 @@ class PurchaseOrderXLSX(orm.Model):
                             _logger.warning(
                                 'Problem convert float / empty: %s' % quantity)
                             quantity = 0.0
-                            
+
                         if not quantity:
                             continue
-                        
+
                         month = month_db[col]
                         day = '01' # TODO Parameterize (wizard data)
                         if month in ['09', '10', '11', '12']:
@@ -359,10 +364,10 @@ class PurchaseOrderXLSX(orm.Model):
                             year = year_b
 
                         # Note: Deadline minimun today!
-                        deadline = '%s-%s-%s' % (year, month, day)                        
+                        deadline = '%s-%s-%s' % (year, month, day)
                         if deadline < today:
                             deadline = today
-                        
+
                         line_pool.create(cr, uid, {
                             'xlsx_id': ids[0],
                             'product_id': product_id,
@@ -379,13 +384,13 @@ class PurchaseOrderXLSX(orm.Model):
                         purchase_lot_block = WS.cell(row, 15).value
                         if type(purchase_lot_block) != float:
                             purchase_lot_block = 0
-                        
+
                         product_pool.write(cr, uid, product_id, {
                             'leadtime': leadtime,
                             'purchase_lot_block': purchase_lot_block,
                             }, context=context)
 
-                    if update_inventory_category and WS.cell(row, 16).value:                        
+                    if update_inventory_category and WS.cell(row, 16).value:
                         inventory_category_id = inventory_db.get(
                             WS.cell(row, 16).value, False)
                         if inventory_category_id:
@@ -399,10 +404,10 @@ class PurchaseOrderXLSX(orm.Model):
                             product_pool.write(cr, uid, product_id, {
                                 'status': 'obsolete',
                                 }, context=context)
-                                
+
                 elif pos in (7, 8) and not WS.cell(row, 0).value:
                     _logger.warning('Reset pos, row: %s' % row)
-                    pos = -1            
+                    pos = -1
 
         # Update status of import record
         _logger.info('Imported: %s' % filename)
@@ -411,18 +416,18 @@ class PurchaseOrderXLSX(orm.Model):
             'file': False, # reset file for clean database!
             'error': error,
             }, context=context)
-    
+
     _columns = {
         'name': fields.char('Name', size=64, required=True),
         'file': fields.binary('XLSX file', filters=None),
         'mode': fields.selection([
             ('draft', 'Draft'),
             ('imported', 'Imported'),
-            ('created', 'Created'),            
+            ('created', 'Created'),
             ], 'Mode'),
         'error': fields.text('Errore'),
         }
-    
+
     _defaults = {
        'name': lambda *a: _('Imported: %s') % datetime.now().strftime(
             DEFAULT_SERVER_DATE_FORMAT),
@@ -432,45 +437,45 @@ class PurchaseOrderXLSX(orm.Model):
 class PurchaseOrderXLSXLine(orm.Model):
     """ Model name: PurchaseOrderXLSX Line
     """
-    
+
     _name = 'purchase.order.xlsx.line'
     _description = 'Imported order line'
     _rec_name = 'product_id'
     _order = 'sequence'
-    
+
     _columns = {
         'sequence': fields.integer('Sequence'),
         'product_id': fields.many2one(
             'product.product', 'Product'),
         'uom_id': fields.related(
-            'product_id', 'uom_id', 
-            type='many2one', relation='product.uom', 
+            'product_id', 'uom_id',
+            type='many2one', relation='product.uom',
             string='UM'),
         'deadline': fields.date('Deadline'),
         'partner_id': fields.many2one('res.partner', 'Supplier'),
         'quantity': fields.float('Q.ty', digits=(16, 3)),
         'xlsx_id': fields.many2one(
-            'purchase.order.xlsx', 'Import wizard', 
+            'purchase.order.xlsx', 'Import wizard',
             required=False),
         }
 
 class PurchaseOrder(orm.Model):
     """ Model name: PurchaseOrder
     """
-    
+
     _inherit = 'purchase.order'
 
     _columns = {
         'xlsx_id': fields.many2one(
-            'purchase.order.xlsx', 'Import record', 
+            'purchase.order.xlsx', 'Import record',
             required=False),
         }
-    
+
 class PurchaseOrderXLSX(orm.Model):
     """ Model name: PurchaseOrderXLSX
-    """    
+    """
     _inherit = 'purchase.order.xlsx'
-    
+
     _columns = {
         'line_ids': fields.one2many(
             'purchase.order.xlsx.line', 'xlsx_id', 'Line'),

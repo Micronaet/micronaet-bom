8b52849a0a7f7b48c01416d19b8cb59d4307f71d
nicothebrush <nicola.riolini@gmail.com>
2020-11-18 11:40:42 +0100

    pep 8 normalitazion

--- lavoration_cl_sl/lavoration.py
+++ lavoration_cl_sl/lavoration.py
@@ -30,39 +30,40 @@ from openerp import SUPERUSER_ID, api
 from openerp import tools
 from openerp.tools.translate import _
 from openerp.tools.float_utils import float_round as round
-from openerp.tools import (DEFAULT_SERVER_DATE_FORMAT, 
-    DEFAULT_SERVER_DATETIME_FORMAT, 
-    DATETIME_FORMATS_MAP, 
+from openerp.tools import (DEFAULT_SERVER_DATE_FORMAT,
+    DEFAULT_SERVER_DATETIME_FORMAT,
+    DATETIME_FORMATS_MAP,
     float_compare)
 
 
 _logger = logging.getLogger(__name__)
 
+
 class StockPickingDevice(orm.Model):
     """ Model name: StockPickingDevice
     """
-    
+
     _name = 'stock.picking.device'
     _description = 'Lavoration device'
-    
+
     _columns = {
         'name': fields.char('Device', size=64, required=True),
         'note': fields.text('Note'),
         }
-    
+
 
 class ResCompany(orm.Model):
     """ Model name: ResCompany
     """
-    
+
     _inherit = 'res.company'
-    
+
     def _get_company_browse(self, cr, uid, context=None):
-        ''' Browse first company for parameter read
-        '''
+        """ Browse first company for parameter read
+        """
         company_ids = self.search(cr, uid, [], context=context)
-        return self.browse(cr, uid, company_ids, context=context)[0]        
-        
+        return self.browse(cr, uid, company_ids, context=context)[0]
+
     _columns = {
         'enable_mrp_lavoration': fields.boolean('Enable lavoration'),
         'sl_mrp_lavoration_id': fields.many2one(
@@ -71,39 +72,42 @@ class ResCompany(orm.Model):
             'stock.picking.type', 'CL Lavoration'),
         }
 
+
 class StockQuant(orm.Model):
     """ Model name: Stock quant
-    """    
+    """
     _inherit = 'stock.quant'
-    
+
     _columns = {
         'lavoration_link_id': fields.many2one(
             'stock.picking', 'Link to CL lavoration', ondelete='cascade'),
         }
 
+
 class StockMove(orm.Model):
     """ Model name: StockMove
-    """    
+    """
     _inherit = 'stock.move'
-    
+
     _columns = {
         # For show direct SL lavoration when confirmed:
         'linked_cl_stock_move_id': fields.many2one(
             'stock.move', 'Link CL linked move', ondelete='set null'),
         }
-        
+
+
 class StockMove(orm.Model):
     """ Model name: StockMove
     """
-    
+
     _inherit = 'stock.move'
-    
+
     # Fields function:
     def _get_linked_sl_status(self, cr, uid, ids, fields, args, context=None):
-        ''' Fields function for calculate 
-        '''
+        """ Fields function for calculate
+        """
         res = {}
-        
+
         for item in self.browse(cr, uid, ids, context=context):
             res[item.id] = ''
             if item.picking_id.state == 'done': # SL movement:
@@ -112,11 +116,11 @@ class StockMove(orm.Model):
                         sl.product_id.default_code or '??',
                         sl.product_qty,
                         )
-            else: # show BOM:
-                for bom in item.product_id.half_bom_ids:                    
+            else:  # show BOM:
+                for bom in item.product_id.half_bom_ids:
                     if item.remove_obtainable and bom.obtainable_component:
-                        _logger.warning('Remove obtainable component: %s' % \
-                            bom.product_id.default_code)
+                        _logger.warning('Remove obtainable component: %s' %
+                                        bom.product_id.default_code)
                         continue
                     res[item.id] += '%s: %s\n' % (
                         bom.product_id.default_code or '??',
@@ -126,31 +130,32 @@ class StockMove(orm.Model):
         return res
 
     _columns = {
-        'product_fail_qty': fields.float('Damaged', 
+        'product_fail_qty': fields.float('Damaged',
             digits_compute=dp.get_precision('Product Unit of Measure')),
-            
+
         'linked_sl_stock_move_id_ids': fields.one2many(
-            'stock.move', 'linked_cl_stock_move_id', 
+            'stock.move', 'linked_cl_stock_move_id',
             'Linked SL movements'),
         'linked_sl_status': fields.function(
-            _get_linked_sl_status, method=True, 
-            type='text', string='SL movement', 
-            store=False),                         
-        'remove_obtainable': fields.boolean('No ricavabili', 
+            _get_linked_sl_status, method=True,
+            type='text', string='SL movement',
+            store=False),
+        'remove_obtainable': fields.boolean('No ricavabili',
             help='Rimuove dalla distinta i componenti ricavabili'),
         }
 
-class MRPLavoration(orm.Model):  
+
+class MRPLavoration(orm.Model):
     """ Manage lavoration as a new object
     """
     _inherit = 'stock.picking'
 
     def get_material_info_from_mrp(self, cr, uid, ids, context=None):
-        ''' Extract info from MRP linked
-        ''' 
+        """ Extract info from MRP linked
+        """
         info = ''
         current_proxy = self.browse(cr, uid, ids, context=context)[0]
-        
+
         res = {}
         for line in current_proxy.linked_mrp_id.order_line_ids:
             product = line.product_id
@@ -169,7 +174,7 @@ class MRPLavoration(orm.Model):
                             <td>%s</td>
                             <td>%s</td>
                         </tr>
-                        ''' % (    
+                        ''' % (
                             product.default_code,
                             qty,
                             hw.category_id.name,
@@ -207,10 +212,10 @@ class MRPLavoration(orm.Model):
         return self.write(cr, uid, ids, {
             'mrp_material_info': info,
             }, context=context)
-                
+
     def force_done(self, cr, uid, ids, context=None):
-        ''' Confirm lavoration
-        '''
+        """ Confirm lavoration
+        """
         # Pool used:
         move_pool = self.pool.get('stock.move')
         quant_pool = self.pool.get('stock.quant')
@@ -219,26 +224,26 @@ class MRPLavoration(orm.Model):
 
         # Read paremeters:
         company_proxy = self.pool.get('res.company')._get_company_browse(
-            cr, uid, context=context)        
-            
+            cr, uid, context=context)
+
         sl_type = company_proxy.sl_mrp_lavoration_id
         sl_type_id = sl_type.id or False
-        
+
         stock_location = sl_type.default_location_src_id.id or False
-        mrp_location = sl_type.default_location_dest_id.id or False 
-        
+        mrp_location = sl_type.default_location_dest_id.id or False
+
         origin = 'CL-LAV-%s' % pick_proxy.name
-        
+
         cl_date = pick_proxy.date # XXX use CL date for all!
-        #datetime.now().strftime(DEFAULT_SERVER_DATE_FORMAT)
-        
+        # datetime.now().strftime(DEFAULT_SERVER_DATE_FORMAT)
+
         # TODO better MRP, now is procurements type?
 
         if not(mrp_location and stock_location):
             raise osv.except_osv(
-                _('Error'), 
+                _('Error'),
                 _('Set up in company location for stock and mrp1!'))
-        
+
         # ---------------------------------------------------------------------
         # Create / Update SL picking:
         # ---------------------------------------------------------------------
@@ -272,7 +277,7 @@ class MRPLavoration(orm.Model):
             product = load.product_id
             load_qty = load.product_qty
             load_fail_qty = load_qty + load.product_fail_qty
-            
+
             # Load quats materials:
             quant_pool.create(cr, uid, {
                 'in_date': cl_date, # TODO document date??
@@ -282,13 +287,13 @@ class MRPLavoration(orm.Model):
                 'qty': load_qty,
                 'lavoration_link_id': pick_proxy.id,
                 }, context=context)
-            
+
             for component in product.half_bom_ids:
                 if load.remove_obtainable and component.obtainable_component:
                     _logger.warning('Remove obtainable stock move: %s' % \
                         component.product_id.default_code)
                     continue
-                    
+
                 product = component.product_id
                 unload_qty = component.product_qty * load_fail_qty
                 if unload_qty <= 0.0:
@@ -302,20 +307,20 @@ class MRPLavoration(orm.Model):
                     'location_dest_id': mrp_location,
                     'picking_type_id': sl_type_id,
                     'product_id': product.id,
-                    'product_uom_qty': unload_qty, 
+                    'product_uom_qty': unload_qty,
                     'product_uom': product.uom_id.id,
                     'state': 'done', # confirmed, available
                     'origin': origin, # CL lavoration
                     'date': cl_date,
                     'date_expected': cl_date,
                     'name': _('SL-LAV-%s') % pick_proxy.name,
-                    'linked_cl_stock_move_id': load.id, # link CL move
-                    #'display_name': 'SL: %s' % line_proxy.product_id.name,
-                    #'product_uom_qty',
-                    #'product_uos',
-                    #'product_uos_qty',
+                    'linked_cl_stock_move_id': load.id,  # link CL move
+                    # 'display_name': 'SL: %s' % line_proxy.product_id.name,
+                    # 'product_uom_qty',
+                    # 'product_uos',
+                    # 'product_uos_qty',
                     }, context=context)
-                    
+
                 # Unload quats materials:
                 quant_pool.create(cr, uid, {
                     'in_date': cl_date,
@@ -325,17 +330,17 @@ class MRPLavoration(orm.Model):
                     'qty': - unload_qty,
                     'lavoration_link_id': pick_proxy.id,
                     }, context=context)
-        
+
         # ---------------------------------------------------------------------
         # Udate CL move status:
         # ---------------------------------------------------------------------
         move_ids = move_pool.search(cr, uid, [
             ('picking_id', '=', ids[0])], context=None)
         move_pool.write(cr, uid, move_ids, {
-            #'partner_id': 
+            # 'partner_id':
             'state': 'done',
             }, context=context)
-            
+
         # ---------------------------------------------------------------------
         # Update header status:
         # ---------------------------------------------------------------------
@@ -345,11 +350,11 @@ class MRPLavoration(orm.Model):
             }, context=context)
 
     def force_draft(self, cr, uid, ids, context=None):
-        ''' Confirm lavoration
-        '''
+        """ Confirm lavoration
+        """
         pick_proxy = self.browse(cr, uid, ids, context=context)[0]
         # Delete load movements:
-        
+
         # Move:
         move_pool = self.pool.get('stock.move')
         quant_pool = self.pool.get('stock.quant')
@@ -361,7 +366,7 @@ class MRPLavoration(orm.Model):
             ('lavoration_link_id', '=', ids[0]),
             ], context=context)
         if quant_ids:
-            quant_pool.unlink(cr, uid, quant_ids, context=context)  
+            quant_pool.unlink(cr, uid, quant_ids, context=context)
 
         # ---------------------------------------------------------------------
         # Delete previous SL movements:
@@ -388,22 +393,22 @@ class MRPLavoration(orm.Model):
             ('picking_id', '=', ids[0])], context=None)
         move_pool.write(cr, uid, stock_ids, {
             'state': 'draft'}, context=context)
-            
-        # Header:        
+
+        # Header:
         return self.write(cr, uid, ids, {
             'state': 'draft',
             }, context=context)
-            
+
     # Default function:
     def _get_picking_type_id(self, cr, uid, context=None):
-        ''' Check value from startup method in context
-        '''       
+        """ Check value from startup method in context
+        """
         if context is None:
             context = {}
-        if not context.get('open_mrp_lavoration', False):   
+        if not context.get('open_mrp_lavoration', False):
             return False
         company_proxy = self.pool.get('res.company')._get_company_browse(
-            cr, uid, context=context)        
+            cr, uid, context=context)
         return company_proxy.cl_mrp_lavoration_id.id or False
         # TODO check error on false
 
@@ -414,12 +419,12 @@ class MRPLavoration(orm.Model):
         'workers': fields.char('Workers', size=64),
         'is_mrp_lavoration': fields.boolean('Is Lavoration'), # TODO remove!!!!
         'device_id': fields.many2one('stock.picking.device', 'Device'),
-        
+
         # Override:
         'picking_type_id': fields.many2one(
-            'stock.picking.type', 'Picking Type', 
+            'stock.picking.type', 'Picking Type',
             states={
-                'done': [('readonly', True)], 
+                'done': [('readonly', True)],
                 'cancel': [('readonly', True)],
                 }, required=True),
         'linked_mrp_id': fields.many2one('mrp.production', 'MRP linked'),
@@ -430,24 +435,23 @@ class MRPLavoration(orm.Model):
         'dep_mode': fields.selection([
             ('cut', 'Cut department'),
             ('workshop', 'Workshop department'),
-            ], 'Department mode', readonly=True),                        
-        }       
-    
+            ], 'Department mode', readonly=True),
+        }
+
     _defaults = {
         'picking_type_id': lambda s, cr, uid, ctx: s._get_picking_type_id(
             cr, uid, ctx),
         }
 
+
 class MrpProduction(orm.Model):
     """ Model name: MrpProduction
     """
-    
+
     _inherit = 'mrp.production'
-    
+
     _columns = {
         'linked_picking_ids': fields.one2many(
-            'stock.picking', 'linked_mrp_id', 
+            'stock.picking', 'linked_mrp_id',
             'Cut'),
     }
-    
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

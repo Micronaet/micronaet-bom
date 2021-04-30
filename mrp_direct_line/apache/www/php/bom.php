34482aeadad2bb5c5f0969130f04742a6a7a0398
nicothebrush <nicola.riolini@gmail.com>
2021-04-30 23:45:25 +0200

    debug operation

--- mrp_direct_line/apache/www/php/bom.php
+++ mrp_direct_line/apache/www/php/bom.php
@@ -12,7 +12,7 @@
     $user = 'admin';
     $password = 'cgp.fmsp6';
     $dbname = 'Fiam';
-    $server = '192.168.1.181';
+    $server = 'localhost';
     $port = '8069';
     $type_connection = 'http';
     $server_url = "http://$server:$port/xmlrpc";

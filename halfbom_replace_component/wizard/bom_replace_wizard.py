f7f436e76c0aa53ccdc5568c2ebcdca9fbe4d9cf
nicothebrush <nicola.riolini@gmail.com>
2021-10-07 18:52:32 +0200

    [ADD] mrp_total_report: Manage total report data

--- halfbom_replace_component/wizard/bom_replace_wizard.py
+++ halfbom_replace_component/wizard/bom_replace_wizard.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ###############################################################################
 #
-# ODOO (ex OpenERP) 
+# ODOO (ex OpenERP)
 # Open Source Management Solution
 # Copyright (C) 2001-2015 Micronaet S.r.l. (<http://www.micronaet.it>)
 # Developer: Nicola Riolini @thebrush (<https://it.linkedin.com/in/thebrush>)
@@ -12,7 +12,7 @@
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 # See the GNU Affero General Public License for more details.
 #
 # You should have received a copy of the GNU Affero General Public License
@@ -32,9 +32,9 @@ from dateutil.relativedelta import relativedelta
 from openerp import SUPERUSER_ID
 from openerp import tools
 from openerp.tools.translate import _
-from openerp.tools import (DEFAULT_SERVER_DATE_FORMAT, 
-    DEFAULT_SERVER_DATETIME_FORMAT, 
-    DATETIME_FORMATS_MAP, 
+from openerp.tools import (DEFAULT_SERVER_DATE_FORMAT,
+    DEFAULT_SERVER_DATETIME_FORMAT,
+    DATETIME_FORMATS_MAP,
     float_compare)
 
 
@@ -48,20 +48,20 @@ class MrpBomReplaceHwWizard(orm.TransientModel):
 
     # -------------------------------------------------------------------------
     # Utility:
-    # -------------------------------------------------------------------------            
+    # -------------------------------------------------------------------------
     def action_run_in_context_mode(self, cr, uid, ids, context=None):
         ''' Event for button print
         '''
-        if context is None: 
+        if context is None:
             context = {}
         mode = context.get('wizard_mode', 'select')
 
-        # Pool used:        
+        # Pool used:
         bom_line_pool = self.pool.get('mrp.bom.line')
         excel_pool = self.pool.get('excel.writer')
 
         wiz_proxy = self.browse(cr, uid, ids, context=context)[0]
-        
+
         # Read parameter:
         select_id = wiz_proxy.select_id.id
         update_id = wiz_proxy.update_id.id
@@ -81,27 +81,27 @@ class MrpBomReplaceHwWizard(orm.TransientModel):
         bom_ids = [line.bom_id.id for line in bom_line_pool.browse(
             cr, uid, bom_line_ids, context=context)]
         bom_ids = list(set(bom_ids))
-        
+
         # ---------------------------------------------------------------------
-        # Report mode: 
+        # Report mode:
         # ---------------------------------------------------------------------
         if mode == 'excel':
             # Create Excel file:
             ws_name = 'Modificati'
             excel_pool.create_worksheet(ws_name)
-            
+
             # Format:
             excel_pool.set_format()
             f_title = excel_pool.get_format('title')
             f_header = excel_pool.get_format('header')
             f_text = excel_pool.get_format('text')
             f_number = excel_pool.get_format('number')
-            
+
             # Column setup:
             excel_pool.column_width(ws_name, [
                 4, 15, 40, 30, 10, 30, 10])
-            
-            # Write header:    
+
+            # Write header:
             row = 0
             from_code = wiz_proxy.select_id.default_code
             to_code = wiz_proxy.update_id.default_code
@@ -109,52 +109,52 @@ class MrpBomReplaceHwWizard(orm.TransientModel):
                 'Aggiorna DB semilavorato, prodotto da %s a %s, con q. %s' % (
                     from_code,
                     to_code,
-                    qty or '/',                    
+                    qty or '/',
                     )
                 ], default_format=f_title)
-            
+
             row += 1
             excel_pool.write_xls_line(ws_name, row, [
                 'ID',
                 'DB Codice',
                 'DB Nome',
-                'Vecchio Componente', 
-                'Vecchia Q.', 
-                'Nuovo componente', 
-                'Nuova Q.' if qty else '',     
+                'Vecchio Componente',
+                'Vecchia Q.',
+                'Nuovo componente',
+                'Nuova Q.' if qty else '',
                 ], default_format=f_header)
-            
-            
-            for line in  bom_line_pool.browse(cr, uid, bom_line_ids, 
+
+
+            for line in  bom_line_pool.browse(cr, uid, bom_line_ids,
                     context=context):
                 row += 1
                 excel_pool.write_xls_line(ws_name, row, [
                     line.id,
                     line.bom_id.product_tmpl_id.default_code,
                     line.bom_id.product_tmpl_id.name,
-                    line.product_id.default_code, 
-                    line.product_qty, 
-                    to_code, 
+                    line.product_id.default_code,
+                    line.product_qty,
+                    to_code,
                     (qty or '/', f_number),
                     ], default_format=f_text)
             return excel_pool.return_attachment(
                 cr, uid, 'Esito modifica', context=context)
-        
+
         # ---------------------------------------------------------------------
-        # Update mode: 
+        # Update mode:
         # ---------------------------------------------------------------------
         if mode == 'update':
             data = {}
             if update_id:
                 data['product_id'] = update_id
             if qty:
-                data['product_qty'] = qty 
+                data['product_qty'] = qty
             if data:
                 bom_line_pool.write(
                     cr, uid, bom_line_ids, data, context=context)
 
         # ---------------------------------------------------------------------
-        # Both select of update   
+        # Both select of update
         # ---------------------------------------------------------------------
         return {
             'type': 'ir.actions.act_window',
@@ -173,30 +173,30 @@ class MrpBomReplaceHwWizard(orm.TransientModel):
 
     # -------------------------------------------------------------------------
     # Wizard button event:
-    # -------------------------------------------------------------------------            
+    # -------------------------------------------------------------------------
     def action_select(self, cr, uid, ids, context=None):
         ''' Select BOM with this filter
         '''
-        if context is None: 
+        if context is None:
             context = {}
-        context['wizard_mode'] = 'select'        
-        return self.action_run_in_context_mode(cr, uid, ids, context=context)    
+        context['wizard_mode'] = 'select'
+        return self.action_run_in_context_mode(cr, uid, ids, context=context)
 
     def action_excel(self, cr, uid, ids, context=None):
         ''' Select BOM with this filter
         '''
-        if context is None: 
+        if context is None:
             context = {}
-        context['wizard_mode'] = 'excel'        
-        return self.action_run_in_context_mode(cr, uid, ids, context=context)    
+        context['wizard_mode'] = 'excel'
+        return self.action_run_in_context_mode(cr, uid, ids, context=context)
 
     def action_update(self, cr, uid, ids, context=None):
         ''' Update BOM with this filter
         '''
-        if context is None: 
+        if context is None:
             context = {}
-        context['wizard_mode'] = 'update'        
-        return self.action_run_in_context_mode(cr, uid, ids, context=context)    
+        context['wizard_mode'] = 'update'
+        return self.action_run_in_context_mode(cr, uid, ids, context=context)
 
     _columns = {
         'select_id': fields.many2one(
@@ -205,4 +205,3 @@ class MrpBomReplaceHwWizard(orm.TransientModel):
             'product.product', 'Update'),
         'qty': fields.float('New Q.', digits=(16, 3)),
         }
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ mrp_total_report/models/total_report.py
@@ -0,0 +1,152 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+# ODOO (ex OpenERP)
+# Open Source Management Solution
+# Copyright (C) 2001-2015 Micronaet S.r.l. (<https://micronaet.com>)
+# Developer: Nicola Riolini @thebrush (<https://it.linkedin.com/in/thebrush>)
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+import os
+import sys
+import logging
+import xlsxwriter
+import openerp
+import openerp.netsvc as netsvc
+import openerp.addons.decimal_precision as dp
+from openerp.osv import fields, osv, expression, orm
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+from openerp import SUPERUSER_ID, api
+from openerp import tools
+from openerp.tools.translate import _
+from openerp.tools.float_utils import float_round as round
+from openerp.tools import (DEFAULT_SERVER_DATE_FORMAT,
+    DEFAULT_SERVER_DATETIME_FORMAT,
+    DATETIME_FORMATS_MAP,
+    float_compare)
+
+_logger = logging.getLogger(__name__)
+
+
+class ResCompany(orm.Model):
+    """ Model name: Company report
+    """
+    _inherit = 'res.company'
+
+    def mrp_total_report(self, cr, uid, ids, context=None):
+        """ Total report
+        """
+        # ---------------------------------------------------------------------
+        # Utility:
+        # ---------------------------------------------------------------------
+        def get_product_touched(self, cr, uid, context=None):
+            """ Get product touched in OC and MRP items
+            """
+            # todo
+            return True
+
+        def get_purchased_material(self, cr, uid, context=None):
+            """ Get list of purchase materia avaiting delivery
+            """
+            # todo
+            return True
+
+        def generate_header(weeks):
+            """ Generate header for total report
+            """
+            header = [
+                'Livello', 'Prodotto', 'Nome',
+            ]
+            columns = [5, 20, 35]
+            fixed_col = len(columns)
+
+            day = datetime.now()
+            for week in range(weeks):
+                isocalendar =  day.isocalendar()[1]
+                header.append('Y%s-W%s' % isocalendar[:2])
+                columns.append(10)
+                day += timedelta(days=7)
+            return header, columns, fixed_col
+
+        # ---------------------------------------------------------------------
+        # Start procedure:
+        # ---------------------------------------------------------------------
+        if context is None:
+            context = {}
+
+        # Add status for calc. HW only (filter)?
+        user_pool = self.pool.get('res.users')
+        previous_status = user_pool.set_no_inventory_status(
+            cr, uid, value=False, context=context)
+
+        # Read parameter for generate structure:
+        company = self.browse(cr, uid, ids[0], context=context)
+        total_week = company.total_report_week
+
+        # Generate datasheet structure:
+        header, columns, fixed_col = generate_header(total_week)
+
+        # ---------------------------------------------------------------------
+        # Collect data:
+        # ---------------------------------------------------------------------
+        total_report = {}
+
+        products = get_product_touched(
+            self, cr, uid, context=context)
+        purchase_material = get_purchased_material(
+            self, cr, uid, context=context)
+
+        # ---------------------------------------------------------------------
+        # Generate report
+        # ---------------------------------------------------------------------
+        excel_pool = self.pool.get('excel.writer')
+        ws_name = 'Gestione MRP'
+        excel_pool.create_worksheet(ws_name)
+
+        # Format:
+        excel_pool.set_format()
+        xls_format = {
+            'white': {  # normal white background
+                'title': excel_pool.get_format('title'),
+                'header': excel_pool.get_format('header'),
+                'text': excel_pool.get_format('text'),
+                'number': excel_pool.get_format('number'),
+            },
+        }
+
+        # Column setup:
+        excel_pool.column_width(ws_name, columns)
+        row = 0
+        excel_pool.write_xls_line(
+            ws_name, row, header, default_format=xls_format['white']['header'])
+
+        for product in products:
+            row += 1
+            row_data = []
+            excel_pool.write_xls_line(
+                ws_name, row, row_data,
+                default_format=xls_format['white']['header'])
+
+        # Restore previous state:
+        user_pool.set_no_inventory_status(
+            cr, uid, value=previous_status, context=context)
+
+        return excel_pool.return_attachment(
+            cr, uid, 'Total report', context=context)
+
+

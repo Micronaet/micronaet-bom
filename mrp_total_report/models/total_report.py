27cb603e26c44d0180999f2a2af94b582c84808d
nicothebrush <nicola.riolini@gmail.com>
2021-10-07 21:17:40 +0200

    generic bugfix (fast message)

--- mrp_total_report/models/total_report.py
+++ mrp_total_report/models/total_report.py
@@ -113,7 +113,7 @@ class ResCompany(orm.Model):
 
         # Generate datasheet structure:
         header, columns, fixed_col = generate_header(total_week)
-        total_col = fixed_col + total_week
+        total_col = fixed_col + total_week - 1
 
         # ---------------------------------------------------------------------
         # Collect data:
@@ -147,7 +147,7 @@ class ResCompany(orm.Model):
         row = 0
         excel_pool.write_xls_line(
             ws_name, row, header, default_format=xls_format['header'])
-        excel_pool = excel_pool.autofilter(ws_name, row, 0, row, total_col)
+        excel_pool.autofilter(ws_name, row, 0, row, total_col)
 
         for product in sorted(total_report,
                               key=lambda p: (p.family_id.name, p.default_code)

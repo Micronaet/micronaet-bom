93cc0a4a48cd8d63c1f0439c4e64c23689e36d46
nicothebrush <nicola.riolini@gmail.com>
2021-10-08 20:31:00 +0200

    generic bugfix (fast message)

--- mrp_total_report/models/total_report.py
+++ mrp_total_report/models/total_report.py
@@ -377,7 +377,6 @@ class ResCompany(orm.Model):
                         sw_week_data = week_data
                     # Cover with stock (all week range block):
                     cover_position = 0
-                    # sw_comment_data = []
                     while stock_status[semiworked] > 0.0 and \
                             cover_position < total_week:
                         needed_qty = sw_week_data[cover_position]
@@ -389,14 +388,18 @@ class ResCompany(orm.Model):
                         if stock_qty > needed_qty:
                             sw_week_data[cover_position] = 0.0
                             stock_status[semiworked] -= needed_qty
-                            # sw_comment_data.append(
-                            #    'Coperta da mag.: %s\n' % needed_qty)
+                            sw_comment_text = 'Coperta da mag.: %s\n' % \
+                                              needed_qty
                         elif stock_qty < needed_qty:
                             # not enough used all remain stock:
                             sw_week_data[cover_position] -= stock_qty
                             stock_status[semiworked] = 0.0  # used all av.!
-                            # sw_comment_data.append(
-                            #    'Coperta da mag.: %s\n' % stock_qty)
+                            sw_comment_text = 'Coperta da mag.: %s\n' % \
+                                stock_qty
+                        excel_pool.write_comment_line(
+                            ws_name, row, cover_position + fixed_col,
+                            sw_comment_text,
+                            parameters=parameters)
                         cover_position += 1
 
                     # ---------------------------------------------------------
@@ -405,9 +408,6 @@ class ResCompany(orm.Model):
                         ws_name, row, sw_week_data,
                         default_format=xls_format['white']['number'],
                         col=fixed_col)
-                    # excel_pool.write_comment_line(
-                    #    ws_name, row, sw_comment_data, col=fixed_col,
-                    #    parameters=parameters)
 
         # Restore previous state:
         user_pool.set_no_inventory_status(

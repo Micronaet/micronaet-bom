7562d6ddbd63de3a60ce78b638a654726092d319
nicothebrush <nicola.riolini@gmail.com>
2022-05-27 13:21:55 +0200

    generic bugfix (fast message)

--- bom_industrial_cost_report/wizard/open_report_wizard.py
+++ bom_industrial_cost_report/wizard/open_report_wizard.py
@@ -250,12 +250,12 @@ class ProductBomReportLimitWizard(orm.TransientModel):
             from_cell = excel_pool.rowcol_to_cell(row + 2, col)
             to_cell = excel_pool.rowcol_to_cell(row + total_row, col)
             formula = u"=SUBTOTAL(9,%s:%s)" % (from_cell, to_cell)
-            excel_pool.write_formula(
-                ws_name,
-                row, col, formula,
-                excel_format['white']['number'],
-                0.0,  # complete_total[position],
-            )
+            #excel_pool.write_formula(
+            #    ws_name,
+            #    row, col, formula,
+            #    excel_format['white']['number'],
+            #    0.0,  # complete_total[position],
+            #)
 
         return excel_pool.return_attachment(cr, uid, 'Comparativo fatturato')
 

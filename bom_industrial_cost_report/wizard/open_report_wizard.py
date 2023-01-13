44a2c6eabe2c3b6cd499b9fef06ced75f5add047
nicothebrush <nicola.riolini@gmail.com>
2023-01-13 10:13:10 +0100

    generic bugfix (fast message)

--- bom_industrial_cost_report/wizard/open_report_wizard.py
+++ bom_industrial_cost_report/wizard/open_report_wizard.py
@@ -429,7 +429,7 @@ class ProductBomReportLimitWizard(orm.TransientModel):
             )
         excel_pool.write_formula(
             ws_name,
-            row, 16, u'= 100 * P1 / O1',
+            row, 16, u'= 100 * Q1 / P1',
             excel_format['green']['number'],
             0.0,  # complete_total[position],
         )

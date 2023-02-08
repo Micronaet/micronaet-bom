227d359020456c4f3be9e86115869ee409782283
nicothebrush <nicola.riolini@gmail.com>
2023-02-08 11:52:37 +0100

    generic bugfix (fast message)

--- order_bom_explode_report/report/mrp_parser.py
+++ order_bom_explode_report/report/mrp_parser.py
@@ -203,7 +203,6 @@ class MrpBomInherit(orm.Model):
         if hidden_rows:  # All green lines!
             excel_pool.row_hidden(ws_name, hidden_rows)
 
-        pdb.set_trace()
         # return self.group\n self.group =
         return excel_pool.send_mail_to_group(
             cr, uid,

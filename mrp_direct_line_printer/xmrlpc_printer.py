d8e2ae8f4078cf5211cae51898a74e638a290170
nicothebrush <nicola.riolini@gmail.com>
2023-05-19 17:23:24 +0200

    generic bugfix (fast message)

--- mrp_direct_line_printer/xmrlpc_printer.py
+++ mrp_direct_line_printer/xmrlpc_printer.py
@@ -122,7 +122,7 @@ class MrpProductionXmlrpcAgent(orm.Model):
         """
         res = ''
         for c in value:
-            if ord(c) <127:
+            if ord(c) < 127:
                 res += c
             else:
                 res += '#'

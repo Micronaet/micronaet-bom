a7708f969d6ca7198db0262da6391949b6391ffb
nicothebrush <nicola.riolini@gmail.com>
2024-07-12 09:37:52 +0200

    generic bugfix (fast message)

--- bom_check_problem_wizard/wizard/bom_report_wizard.py
+++ bom_check_problem_wizard/wizard/bom_report_wizard.py
@@ -376,7 +376,7 @@ class MrpBomCheckProblemWizard(orm.TransientModel):
                 # -------------------------------------------------------------
                 q_x_pack = int(product.q_x_pack)
                 is_single = default_code[12:13] == 'S'
-                if not q_x_pack or (is_single and q_x_pack == 1):
+                if not q_x_pack or (is_single and q_x_pack != 1):
                     check_single_color = cell_format['bg']['red']
                 else:
                     check_single_color = cell_format['bg']['green']
